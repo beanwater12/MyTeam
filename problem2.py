eb1528705b677291bedc7c3864e490d1e4cd9774
dania-ali <10dania.ali@gmail.com>
2025-02-24 23:36:20 +0000

    Finished problem 2

--- problem2.py
+++ problem2.py
@@ -1,1 +1,15 @@
+def factorial (n):
+    #Edge cases - number is negative
+    if n < 0:
+        return "Not defined for negative numbers"
+    #one and zero have 1 as facotrial
+    elif n == 0 or n == 1
+        return 1
+    else:
+        result = 1
+        #start from 2 because we already did cases for 0 and 1
+        for i in range(2, n+1):
+            result *= i
+        return result
 
+print(factorial(4))  # Output: 24
